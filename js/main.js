476270375bab60d8e70f1d23762e3d18c0e089ca
turtleDev <saravjeetamansingh@gmail.com>
2016-04-02 13:16:32 +0530

    add WIP on version 1

--- js/main.js
+++ js/main.js
@@ -4,18 +4,45 @@
  * lights, camera, action!
  */
 (function() {
+    var director = "turtleDev";
+    var chair = document.querySelector('.the-dude');
+
+    scramble.createEmpty(chair, director.length);
+    scramble.setText(chair, director);
+    setTimeout(function() {
+        scramble.descramble(chair);
+    }, 1000);
+
+    var ticket = "info";
+    var ticketCounter = document.querySelector('.info a');
+
+    setTimeout(function() {
+        scramble.createEmpty(ticketCounter, ticket.length);
+        scramble.setText(ticketCounter, ticket);
+        scramble.descramble(ticketCounter);
+    }, 8000);
+
+    ticketCounter.addEventListener("mouseenter", function(e) {
+        scramble.enscramble(e.target);
+    });
+
+    ticketCounter.addEventListener("mouseleave", function(e) {
+        scramble.descramble(e.target);
+    });
+
     var script = [{
         text: "Awesomeness",
-        delay: 1,
+        delay: 3,
         duration: 0.7
     }, {
         text: ", coming soon to a computer near you",
         delay: 1.5,
-        duration: 1.5
+        duration: 1.2
     }];
+
     var preShowCommercial = 1;
 
-    var stage = document.querySelector('.js-message');
+    var stage = document.querySelector('.message');
 
     script.reduce(function(info, scene) {
         var dialogues = scene.text.split('');
---
+++ js/scramble.js
@@ -0,0 +1,203 @@
+/**
+ *
+ * The MIT License
+ *
+ * Copyright (c) 2016 Saravjeet 'Aman' Singh
+ * <saravjeetamansingh@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+'use strict';
+
+(function(module) {
+    var exports = Object.create(null);
+
+    var MIN_FLIP = 1;
+    var MAX_FLIP = 7;
+
+    var MIN_INTERVAL = 70;
+    var MAX_INTERVAL = 140;
+
+    var MIN_DELAY = 10;
+    var MAX_DELAY = 50;
+
+    var chars = [ "a",  "b",  "c",  "d",  "e",  "f",  "g",  "h",  "i",  "j",  "k",  "l",  "m",  "n",  "o",  "p",  "q",  "r",  "s",  "t",  "u",  "v",  "w",  "x",  "y",  "z",  "0",  "1",  "2",  "3",  "4",  "5",  "6",  "7",  "8",  "9",  "0",  "&excl;",  "&quest;",  "&midast;",  "&lpar;",  "&rpar;",  "&commat;",  "&pound",  "&dollar;",  "&percnt;",  "&Hat;",  "&amp;",  "&UnderBar;",  "-",  "&plus;",  "&equals;",  "&lsqb;",  "&rbrack;",  "&lcub;",  "&rcub;",  "&colon;",  "&semi;",  "&bsol;",  "&apos;",  "&QUOT;",  "&VerticalLine;",  "&LT",  "&GT",  "&comma;",  "&period;",  "&sol;",  "~",  "&DiacriticalGrave;",  "&NewLine;", ];
+
+    function newPlaceholder(ch) {
+        return "<span data-ch='" + ch + "'>" + ch + "</span>";
+    }
+
+    function scaffold(el) {
+
+        if (el.children.length !== 0) {
+            return;
+        }
+
+        var baseString = el.innerHTML.split('');
+        var replacement = "";
+        baseString.forEach(function(c) {
+            replacement += newPlaceholder(c);
+        });
+        
+        el.innerHTML = replacement;
+        el.className += " js-scramble";
+    }
+
+    function _getRandomChar() {
+        var index = Math.floor(Math.random() * chars.length);
+        return chars[index];
+    }
+
+    function getRandomChars(n) {
+        /* get a list of n random chars */
+        var list = [];
+        for ( var i = 0; i < n; ++i ) {
+            list.push(_getRandomChar());
+        }
+        return list;
+    }
+
+    function getRandomValue(min, max) {
+        /* get a list of random values between min and max */
+        return min + Math.floor(Math.random() * (max - min));
+    }
+
+    function hasClass(el, cls) {
+        return el.className.indexOf(cls) != -1;
+    }
+
+    function domListToArray(dl) {
+        /* convert DOM list to an Array. Doesn't work for for IE8 or less */
+        return [].slice.call(dl, 0);
+    }
+
+    function queueAnimation(el, cb) {
+        /* cb takes the arguments (el, chars, id) */
+
+        if ( !hasClass(el, "js-scramble") ) {
+            scaffold(el);
+        }
+
+        var actors = domListToArray(el.children);
+        actors.forEach(function(actor) {
+            var delay = getRandomValue(MIN_DELAY, MAX_DELAY);
+            setTimeout(function() {
+                var n = getRandomValue(MIN_FLIP, MAX_FLIP);
+                var ch = getRandomChars(n);
+                var interval = getRandomValue(MIN_INTERVAL, MAX_INTERVAL);
+                var id = setInterval(function() {
+                    cb(actor, ch, id);
+                }, interval);
+            }, delay);
+        });
+    }
+
+    exports.enscramble = function(el) {
+        queueAnimation(el, function(actor, chars, id) {
+            if ( chars.length == 0 ) {
+                clearInterval(id);
+                return;
+            }
+            actor.innerHTML = chars.pop();
+        });
+    }
+
+    exports.descramble = function(el) {
+        queueAnimation(el, function(actor, chars, id) {
+            if ( chars.length == 0 ) {
+                clearInterval(id);
+                actor.innerHTML = actor.dataset.ch;
+                return;
+            }
+            actor.innerHTML = chars.pop();
+        });
+    }
+
+    exports.setText = function(el, text) {
+
+        if ( typeof text != "string" ) {
+            console.error("scramble.setText: text must be a string, got: " + typeof text);
+            return;
+        }
+
+        if ( !hasClass(el, "js-scramble") ) {
+            scaffold(el);
+        }
+
+        var actors = el.children;
+        for ( var i = 0; i < actors.length; ++i ) {
+            actors[i].dataset.ch = text[i] || "&nbsp;";
+        }
+    }
+
+    exports.createEmpty = function(el, length) {
+
+        if ( typeof length != "number" ) {
+            console.error("scramble.createEmpty: length must be a number, got: " + typeof length);
+            return;
+        }
+
+        var components = "";
+        for ( var i = 0; i < length; ++ i ) {
+            components += newPlaceholder("&nbsp;");
+        }
+
+        el.innerHTML = components;
+        
+        if ( !hasClass(el, "js-scramble") ) {
+            el.className += " js-scramble";
+        }
+    }
+
+    if ( module.jQuery ) {
+        module.jQuery.fn.scramble = function(action, arg) {
+            
+            /* get list of selected DOM elements */
+            var els = this.get();
+
+            if ( action == undefined || action == "enscramble" ) {
+                els.forEach(function(el) {
+                    exports.enscramble(el);
+                });
+            } else if ( action == "descramble" ) {
+                els.forEach(function(el) {
+                    exports.descramble(el);
+                });
+            } else if ( action == "setText" ) {
+                els.forEach(function(el) {
+                    exports.setText(el, arg);
+                });
+            } else if ( action == "createEmpty" ) {
+                els.forEach(function(el) {
+                    exports.createEmpty(el, arg);
+                });
+            } else if ( action == "export" ) {
+                return exports;
+            } else {
+                console.error("scramble: unrecognized operation: " + action);
+            }
+
+            return this;
+        }
+    } else {
+        module.scramble = exports
+    }
+})(this)
